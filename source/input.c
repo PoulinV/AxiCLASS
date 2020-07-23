dc25469b1e7f2ed1bb739653cc0748d30ee0784f
Vivian POULIN <vpoulin@GalaxyWatchActive2-8D33.home>
2020-07-23 23:13:13 +0200

    debug

--- source/input.c
+++ source/input.c
@@ -380,17 +380,17 @@ class_call(parser_read_string(pfc,"do_shooting",&string1,&flag1,errmsg),
         else {
           fzw.scf_evolve_as_fluid = _FALSE_;
         }
-       class_call(parser_read_string(pfc,"scf_evolve_as_fluid_always",&string1,&flag1,errmsg),
-                 errmsg,
-                 errmsg);
-
-        if ((flag1 == _TRUE_) && ((strstr(string1,"y") != NULL) || (strstr(string1,"Y") != NULL))) {
-        fzw.scf_evolve_as_fluid_always = _TRUE_;
-        pba->scf_evolve_as_fluid = _TRUE_;
-        }
-        else {
-          fzw.scf_evolve_as_fluid_always = _FALSE_;
-        }
+       // class_call(parser_read_string(pfc,"scf_evolve_as_fluid_always",&string1,&flag1,errmsg),
+       //           errmsg,
+       //           errmsg);
+       //
+       //  if ((flag1 == _TRUE_) && ((strstr(string1,"y") != NULL) || (strstr(string1,"Y") != NULL))) {
+       //  fzw.scf_evolve_as_fluid_always = _TRUE_;
+       //  pba->scf_evolve_as_fluid = _TRUE_;
+       //  }
+       //  else {
+       //    fzw.scf_evolve_as_fluid_always = _FALSE_;
+       //  }
 
 
 
@@ -1718,7 +1718,7 @@ int input_read_parameters(
 	else if (flag3 == _TRUE_) {
 	  pba->log10_a_c = param3;
 	  pba->a_c = pow(10,pba->log10_a_c);
-	} 
+	}
         if(input_verbose>5) printf("Read in a_c = %e\n", pba->a_c);
       }
       // Do we want to shoot for it based on mathing a_peak = a_eq
@@ -1982,26 +1982,26 @@ int input_read_parameters(
     else {
       pba->scf_evolve_as_fluid = _FALSE_;
     }
-    class_call(parser_read_string(pfc,
-                                  "scf_evolve_as_fluid_always",
-                                  &string1,
-                                  &flag1,
-                                  errmsg),
-                errmsg,
-                errmsg);
-
-    if (flag1 == _TRUE_){
-      if((strstr(string1,"y") != NULL) || (strstr(string1,"Y") != NULL)){
-        pba->scf_evolve_as_fluid_always = _TRUE_;
-        pba->scf_evolve_as_fluid = _TRUE_;
-      }
-      else {
-        pba->scf_evolve_as_fluid_always = _FALSE_;
-      }
-    }
-    else {
-      pba->scf_evolve_as_fluid_always = _FALSE_;
-    }
+    // class_call(parser_read_string(pfc,
+    //                               "scf_evolve_as_fluid_always",
+    //                               &string1,
+    //                               &flag1,
+    //                               errmsg),
+    //             errmsg,
+    //             errmsg);
+    //
+    // if (flag1 == _TRUE_){
+    //   if((strstr(string1,"y") != NULL) || (strstr(string1,"Y") != NULL)){
+    //     pba->scf_evolve_as_fluid_always = _TRUE_;
+    //     pba->scf_evolve_as_fluid = _TRUE_;
+    //   }
+    //   else {
+    //     pba->scf_evolve_as_fluid_always = _FALSE_;
+    //   }
+    // }
+    // else {
+    //   pba->scf_evolve_as_fluid_always = _FALSE_;
+    // }
     // class_test(ppt->gauge == newtonian && pba->scf_evolve_as_fluid == _FALSE_,errmsg,
     //   "You are running a scalar field without the fluid approximation however the perturbed KG equation is only written in the synchronous gauge, please switch your gauge!")
 
@@ -4253,7 +4253,7 @@ int input_default_params(
   pba->phi_prime_ini_scf = 1;
   ppt->use_big_theta_scf = _FALSE_;
   pba->scf_evolve_as_fluid = _FALSE_;
-  pba->scf_evolve_as_fluid_always = _FALSE_;
+  // pba->scf_evolve_as_fluid_always = _FALSE_;
   pba->scf_evolve_like_axionCAMB = _FALSE_;
 
   pba->Omega0_k = 0.;
