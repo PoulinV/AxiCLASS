cbfa6d7c316727f1f5daf47d366995d7f4440822
Vivian Poulin <vivian.poulin@umontpellier.fr>
2024-11-29 15:26:05 +0100

    debugging

--- source/perturbations.c
+++ source/perturbations.c
@@ -3133,70 +3133,8 @@ int perturbations_solve(
   // int (*generic_evolver)();
   // auto generic_evolver = &(evolver_ndf15);
 
-  extern int evolver_rk(int (*derivs)(double x,
-				    double * y,
-				    double * dy,
-				    void * parameters_and_workspace,
-				    ErrorMsg error_message),
-		      double x_ini,
-		      double x_end,
-		      double * y,
-		      int * used_in_output,
-		      int y_size,
-		      void * parameters_and_workspace_for_derivs,
-		      double tolerance,
-		      double minimum_variation,
-		      int (*evaluate_timescale)(double x,
-						void * parameters_and_workspace,
-						double * timescale,
-						ErrorMsg error_message),
-		      double timestep_over_timescale,
-		      double * x_sampling,
-		      int x_size,
-		      int (*output)(double x,
-				    double y[],
-				    double dy[],
-				    int index_x,
-				    void * parameters_and_workspace,
-				    ErrorMsg error_message),
-		      int (*print_variables)(double x,
-					     double y[],
-					     double dy[],
-					     void * parameters_and_workspace,
-					     ErrorMsg error_message),
-		      ErrorMsg error_message);
-  extern int evolver_ndf15(int (*derivs)(double x,
-				    double * y,
-				    double * dy,
-				    void * parameters_and_workspace,
-				    ErrorMsg error_message),
-		      double x_ini,
-		      double x_end,
-		      double * y,
-		      int * used_in_output,
-		      int y_size,
-		      void * parameters_and_workspace_for_derivs,
-		      double tolerance,
-		      double minimum_variation,
-		      int (*evaluate_timescale)(double x,
-						void * parameters_and_workspace,
-						double * timescale,
-						ErrorMsg error_message),
-		      double timestep_over_timescale,
-		      double * x_sampling,
-		      int x_size,
-		      int (*output)(double x,
-				    double y[],
-				    double dy[],
-				    int index_x,
-				    void * parameters_and_workspace,
-				    ErrorMsg error_message),
-		      int (*print_variables)(double x,
-					     double y[],
-					     double dy[],
-					     void * parameters_and_workspace,
-					     ErrorMsg error_message),
-		      ErrorMsg error_message);
+  extern int evolver_rk(EVOLVER_PROTOTYPE);
+  extern int evolver_ndf15(EVOLVER_PROTOTYPE);
   int (*generic_evolver)(EVOLVER_PROTOTYPE) = evolver_ndf15;
 
   /* Related to the perturbation output */
@@ -3459,12 +3397,12 @@ int perturbations_solve(
 
     /** - --> (d) integrate the perturbations over the current interval. */
 
-    if (ppr->evolver == rk){
-      generic_evolver = evolver_rk;
-    }
-    else {
+    // if (ppr->evolver == rk){
+    //   generic_evolver = evolver_rk;
+    // }
+    // else {
       generic_evolver = evolver_ndf15;
-    }
+    // }
 
 
     class_call(generic_evolver(perturbations_derivs,
